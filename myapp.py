3711cc183e369b50e36f694d60d053a042255719
Nicolas Bueno Emidio <nicolasbueno.emidio@gmail.com>
2024-04-30 19:42:08 -0300

    branch implementando barra lateral

--- myapp.py
+++ myapp.py
@@ -1,14 +1,39 @@
 # Importando as bibliotecas necessárias
 import openai
 import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+
+CSV_FILE = "dataset_dummy_curso.csv"
+try:
+    chat_history_df = pd.read_csv(CSV_FILE)
+except FileNotFoundError:
+    chat_history_df = pd.DataFrame(columns=["ChatID", "Role", "Content"])
 
 # Verificando se a chave 'key' existe no estado da sessão
 # Se não existir, é inicializado um valor
-if 'key' not in st.session_state:
-    st.session_state['key'] = 'value'
+#if 'key' not in st.session_state:
+#    st.session_state['key'] = 'value'
 
 # Título da interface de Streamlit
-st.title("GPT Protocolos De Inseminação")
+st.title("GPT Analítico")
+
+def get_button_label(chat_df, chat_id):
+    first_message = chat_df[(chat_df["ChatID"] == chat_id) & (chat_df["Role"] == "user")].iloc[0]["Content"]
+    return f"Chat {chat_id[0:7]}: {' '.join(first_message.split()[:5])}..."
+
+
+# Barra lateral e botões, carrega chat ao clicar. 
+# Funcional, GPT corrigiu.
+for chat_id in chat_history_df["ChatID"].unique():
+    button_label = get_button_label(chat_history_df, chat_id)
+    if st.sidebar.button(button_label):
+        current_chat_id = chat_id
+        for i, row in chat_history_df[chat_history_df["ChatID"] == chat_id].iterrows():
+            st.session_state.messages.append({"role": row['Role'], "content": row['Content']})
+            with st.chat_message(row['Role']):
+                st.markdown(row['Content'])
+
 
 # Inicialização da instância do cliente OpenAI com chave de API
 client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
@@ -21,10 +46,19 @@ if "openai_model" not in st.session_state:
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Aqui, ele itera sobre todas as mensagens e as exibe
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+
+## Aqui, ele itera sobre todas as mensagens do CSV e as exibe (teste)
+#for i in range(len(chat_history_df)):
+#    st.session_state.messages.append({"role": chat_history_df['Role'][i], "content": chat_history_df['Content'][i]})
+#    with st.chat_message(chat_history_df["Role"][i]):
+#        st.markdown(chat_history_df["Content"][i])
+
+
+
+## Aqui, ele itera sobre todas as mensagens e as exibe (ORIGINAL)
+#for message in st.session_state.messages:
+#    with st.chat_message(message["role"]):
+#        st.markdown(message["content"])
 
 # Criação de campo de entrada para a mensagem do usuário
 if prompt := st.chat_input("Escreva aqui:"):
